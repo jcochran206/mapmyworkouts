8b2dbe991780cb70bc270f1bf31b2310ec83a992
jcochran206 <jonathan.o.cochran@gmail.com>
2023-05-07 23:11:33 -0700

    added styles, script, index

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('hi');
+//alert('hi');
+
+'use strict';
+
+const months = ["January", "Februrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+
+
