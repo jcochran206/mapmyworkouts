230abeb666747d3b41b2c892fb4154e632364db1
jcochran206 <jonathan.o.cochran@gmail.com>
2023-05-09 23:21:16 -0700

    added geolocation function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //alert('hi');
 
 'use strict';
-
+alert('hi');
 const months = ["January", "Februrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const form = document.querySelector('.form');
@@ -11,6 +11,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+    navigator.geolocation.getCurrentPosition(
+    function(position){
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+        console.log('coordinates in lat and long',latitude, longitude);
+    }, 
+    function(){
+        alert('Can not get position');
+    });
 
 
 
